0be8cf1388b7019e2b6a16440253dc7d3b700949
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2019-09-16 14:30:09 +0200

    Update example executable to compute sums for random partitions

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,11 +1,39 @@
 #include "ClusterEvaluator.h"
+#include <fstream>
+#include <string>
 
 int main()
 {
-  HitClusterInfo a;
-  HitClusterInfo b(4);
-  HitClusterInfo c(2,500);
-  // This will crash
-  HitClusterInfo d(1);
+
+  // parameters
+  unsigned int nombreTotalDeHits=1000;
+  unsigned int nombreDeClusters=12;  
+  unsigned int nombreDeClusterings=10;
+  std::string outputFileName="random_test.txt";
+  
+  //preparation
+  srand(time(NULL));
+  unsigned int hitTab[nombreTotalDeHits];
+  unsigned int *hitPtr=hitTab;
+  unsigned int *clusterPtr=hitPtr;
+  std::ofstream outputFile;
+  outputFile.open(outputFileName.c_str());
+
+  //running
+  HitClusterInfo HCI(nombreDeClusterings,nombreTotalDeHits);
+  HCI.setHits(hitPtr,hitPtr+nombreTotalDeHits);
+  for (unsigned int ihit=0; ihit<nombreTotalDeHits; ++ihit)
+    for (unsigned int iclustering=0; iclustering<nombreDeClusterings; ++iclustering)
+      HCI.addClusterCheck(iclustering,hitPtr+ihit,clusterPtr+(rand()%nombreDeClusters));
+
+  std::map<HitClusterInfo::ClusterSetIndices,ClusterPairsDataSums> m=HCI.getAllDataSums();
+
+  for (auto& sum : m) outputFile << sum.second << std::endl;
+
+  //finalising
+  outputFile.close();
+    
+  
+  
   return 0;
 }
