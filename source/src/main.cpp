85c797be1cbdcdf11debc65607e5f1a6f4d8210d
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2019-09-16 14:59:05 +0200

    Add option to ClusterTools_exe executable

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,8 +1,20 @@
 #include "ClusterEvaluator.h"
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <getopt.h>
 
-int main()
+void usage(char* progname)
+{
+  std::cout << "Usage :" << progname << " takes fthe following options : " << std::endl;
+  std::cout << "\t -n X or --number_of_hits=X : specify the number of hits to cluster (default is 1000)"  << std::endl;
+  std::cout << "\t -c X or --number_of_clusters : specify the maximum number of clusters to make (default is 12)" << std::endl;
+  std::cout << "\t -C X or --number_of_clusterings : specify the number of random clustering to perform (default is 10)" << std::endl;
+  std::cout << "\t -f string or --output_file_name=string : the output file name (default is random_test.txt)"  << std::endl;
+  std::cout << "\t -h or --help : print this help and exit"  << std::endl;
+}
+
+int main(int argc, char **argv)
 {
 
   // parameters
@@ -10,6 +22,32 @@ int main()
   unsigned int nombreDeClusters=12;  
   unsigned int nombreDeClusterings=10;
   std::string outputFileName="random_test.txt";
+
+  //option decoding
+  static struct option long_options[] = {
+    {"number_of_hits",        required_argument, 0,  'n' },
+    {"number_of_clusters",    required_argument, 0,  'c' },
+    {"number_of_clusterings", required_argument, 0,  'C' },
+    {"output_file_name",      required_argument, 0,  'f' },
+    {"help",                  no_argument,       0,  'h' },
+    {0,                       0,                 0,  0   }
+  };
+
+  int long_index =0;
+  int opt= 0;
+  while ((opt = getopt_long(argc, argv,"n:c:C:f:h",long_options, &long_index )) != -1)
+    {
+      switch(opt)
+	{
+	case 'n' : nombreTotalDeHits=atoi(optarg); break;
+	case 'c' : nombreDeClusters=atoi(optarg); break;
+	case 'C' : nombreDeClusterings=atoi(optarg); break;
+	case 'f' : outputFileName=optarg; break;
+	case 'h' : usage(argv[0]); return 0;
+	default  : usage(argv[0]); return 1;
+	}
+    }
+
   
   //preparation
   srand(time(NULL));
